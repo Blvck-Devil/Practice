d5c69d3a54ce087c9ee26a0d985d259087ff53f7
Blvck-Devil <112816848+Blvck-Devil@users.noreply.github.com>
2022-09-05 11:27:01 +0000

    lines

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    System.out.println("Mohamed \t Inthisham \t Hameed");
+    System.out.println("From \n Ratnapura");
   }
 }
